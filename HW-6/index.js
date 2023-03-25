8982ab039429122de8b4fc12af88ce572cab47dd
AdminK <volvenkova.k@gmail.com>
2023-03-25 22:33:11 +0200

    correct hw-6

--- HW-6/index.js
+++ HW-6/index.js
@@ -1,35 +1,33 @@
 let userYearOfBirth = prompt("Вкажіть Ваш рік народження");
-let userCity = prompt("Вкажіть в якому місті Ви живете?");
-let userSport = prompt("Вкажіть який Ваш улюблений вид спорту?");
-
 let userAge = "";
 (userYearOfBirth === null || userYearOfBirth.trim() === "" || isNaN(+userYearOfBirth))
-    ? userAge = "Шкода що Ви не захотіли вводити свій рік народження"
-    : userAge = "Ваш вік: " + (2023 - userYearOfBirth);
+    ? alert("Шкода що Ви не захотіли вводити свій рік народження")
+    : userAge = "Ваш вік: " + (2023 - userYearOfBirth) +". ";
 
+let userCity = prompt("Вкажіть в якому місті Ви живете?");
 let userCityMessage = "";
 if (userCity === null || userCity.trim() === "") {
-    userCityMessage = "Шкода що Ви не захотіли вводити назву міста, в якому живете";
+    alert("Шкода що Ви не захотіли вводити назву міста, в якому живете");
 } else if (userCity.toLowerCase() === "київ") {
-    userCityMessage = "Ви живете у столиці України";
+    userCityMessage = " Ви живете у столиці України.";
 } else if (userCity.toLowerCase() === "вашингтон") {
-    userCityMessage = "Ви живете у столиці США";
+    userCityMessage = " Ви живете у столиці США.";
 } else if (userCity.toLowerCase() === "лондон") {
-    userCityMessage = "Ви живете у столиці Великобританії";
+    userCityMessage = " Ви живете у столиці Великобританії.";
 } else {
-    userCityMessage = "Ви живете у місті " + userCity;
+    userCityMessage = " Ви живете у місті " + userCity +".";
 };
 
+let userSport = prompt("Вкажіть який Ваш улюблений вид спорту?");
 let userSportMessage = "";
 if (userSport === null || userSport.trim() === "") {
-    userSportMessage = "Шкода що Ви не захотіли вказати свій улюблений вид спорту.";
+    alert("Шкода що Ви не захотіли вказати свій улюблений вид спорту.");
 } else if (userSport.toLowerCase() === "бокс") {
-    userSportMessage = "Круто! Хочете стати як Мухамед Алі?";
+    userSportMessage = " Круто! Хочете стати як Мухамед Алі?";
 } else if (userSport.toLowerCase() === "плавання") {
-    userSportMessage = "Круто! Хочете стати як Майкл Фелпс?";
+    userSportMessage = " Круто! Хочете стати як Майкл Фелпс?";
 } else if (userSport.toLowerCase() === "легка атлетика") {
-    userSportMessage = "Круто! Хочете стати як Усейн Болт?";
+    userSportMessage = " Круто! Хочете стати як Усейн Болт?";
 }
 
-
-alert(userAge + ".\n" + userCityMessage + ".\n" + userSportMessage);
+alert(userAge + userCityMessage + userSportMessage);
--- HW-7/index.js
+++ HW-7/index.js
@@ -1,12 +1,12 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
-if(numOrStr === null) {
-console.log('ви скасували')
-} else if( numOrStr.trim() === '' ) {
-console.log('Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-console.log(' number is Ba_NaN')
+if (numOrStr === null) {
+    console.log('ви скасували')
+} else if (numOrStr.trim() === '') {
+    console.log('Empty String');
+} else if (isNaN(+numOrStr)) {
+    console.log(' number is Ba_NaN')
 } else {
-console.log('OK!')
+    console.log('OK!')
 }
