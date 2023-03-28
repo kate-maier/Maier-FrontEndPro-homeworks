d9329670f82f58c3e9ea77a562fedc8c8cc0d553
AdminK <volvenkova.k@gmail.com>
2023-03-28 21:35:00 +0300

    changes in hw-9

--- HW-9/index.js
+++ HW-9/index.js
@@ -1,100 +1,96 @@
 // Завдання 1
 // Вивести на сторінку в один рядок через кому числа від 10 до 20.
-let i = 10;
-let result = [];
-while (i < 20) {
-    result.push(i);
-    i++;
+let j = 10;
+let numbers = [];
+while (j < 20) {
+    numbers.push(j);
+    j++;
 }
-alert("Числа від 10 до 20 (не включно):\n" + result);
-
+alert("Числа від 10 до 20 (не включно):\n" + numbers);
 
 // Завдання 2
 // Вивести квадрати чисел від 10 до 20.
-i = 10;
-let n = 0;
-result = [];
-while (i < 20) {
-    let n = i ** 2;
-    result.push(n);
-    i++;
+j = 10;
+let k = 0;
+let squaresNumbers = [];
+while (j < 20) {
+    k = j ** 2;
+    squaresNumbers.push(k);
+    j++;
 }
-alert("Квадрати чисел від 10 до 20 (не включно):\n" + result);
+alert("Квадрати чисел від 10 до 20 (не включно):\n" + squaresNumbers);
 
 //Завдання 3
 // Вивести таблицю множення на 7.
-i = 1;
-n = 0;
-result = [];
-while (i <= 10) {
-    n = "7 * " + i + " = " + (i * 7);
-    result.push(n);
-    i++;
+j = 1;
+k = 0;
+let tableMult = [];
+while (j <= 10) {
+    k = "7 * " + j + " = " + (j * 7);
+    tableMult.push(k);
+    j++;
 }
-alert("Таблиця множення на 7:\n" + result.join("\n"));
+alert("Таблиця множення на 7:\n" + tableMult.join("\n"));
 
 // Задання 4
 // Знайти суму всіх цілих чисел від 1 до 15.
-i = 1;
-result = 0;
-while (i < 15) {
-    result += i;
-    i++;
+j = 1;
+let sumNumbers = 0;
+while (j < 15) {
+    sumNumbers += j;
+    j++;
 }
-alert("Сума цілих чисел від 1 до 15(не включно):\n" + result);
+alert("Сума цілих чисел від 1 до 15(не включно):\n" + sumNumbers);
 
 // Завдання 5
 // Знайти добуток усіх цілих чисел від 15 до 35.
-i = 15;
-result = 1;
-while (i < 35) {
-    result *= i;
-    i++;
+j = 15;
+let productNumbers = 1;
+while (j < 35) {
+    productNumbers *= j;
+    j++;
 }
-alert("Добуток усіх цілих чисел від 15 до 35(не включно):\n" + result);
+alert("Добуток усіх цілих чисел від 15 до 35(не включно):\n" + productNumbers);
 
 // Завдання 6
 // Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
-i = 1;
-n = 0;
-result = 0;
-while (i < 500) {
-    n += i;
-    result = n / i;
-    i++;
+j = 1;
+k = 0;
+let averageValue = 0;
+while (j < 500) {
+    k += j;
+    averageValue = k / j;
+    j++;
 }
-alert("Середнє арифметичне всіх цілих чисел від 1 до 500(не включно):\n" + result);
+alert("Середнє арифметичне всіх цілих чисел від 1 до 500(не включно):\n" + averageValue);
 
 // Завдання 7
 // Вивести суму лише парних чисел в діапазоні від 30 до 80.
-i = 30;
-result = 0;
-for (; i < 80; i++) {
+let sumEvenNumbers = 0;
+for (let i = 30; i < 80; i++) {
     if (i % 2 !== 0) {
         continue;
     }
-    result = result + i;
+    sumEvenNumbers = sumEvenNumbers + i;
 }
-alert("Сума лише парних чисел в діапазоні від 30 до 80(не включно):\n" + result);
+alert("Сума лише парних чисел в діапазоні від 30 до 80(не включно):\n" + sumEvenNumbers);
 
 // Завдання 8 
 // Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-i = 100;
-result = [];
-for (; i < 200; i++) {
+let numbersDivThree = [];
+for (let i = 100; i < 200; i++) {
     if (i % 3 !== 0) {
         continue;
     }
-    result.push(i);
+    numbersDivThree.push(i);
 }
-alert("Всі числа в діапазоні від 100 до 200 кратні 3:\n" + result.join(", "));
+alert("Всі числа в діапазоні від 100 до 200 кратні 3:\n" + numbersDivThree.join(", "));
 
 // Завдання 9
 // Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-i = 0;
 let N = 24;
 let divisors = [];
-for (; i <= N; i++) {
+for (let i = 0; i <= N; i++) {
     if ((N % i) !== 0) {
         continue;
     }
@@ -104,52 +100,38 @@ alert("Дільники натурального числа «" + N + "»:\n" + 
 
 // Завдання 10
 // Визначити кількість його парних дільників.
-N = 24;
 let evenDivisors = [];
-for (let x = 0; x <= N; x++) {
-    if ((N % x !== 0) || (x % 2 !== 0)) {
+for (let i = 0; i <= N; i++) {
+    if ((N % i !== 0) || (i % 2 !== 0)) {
         continue;
     }
-    evenDivisors.push(x);
+    evenDivisors.push(i);
 }
 alert("Кількість парних дільників натурального числа «" + N + "»:\n" + evenDivisors.length);
 
 // Завдання 11
 // Знайти суму його парних дільників.
-N = 24;
-n = 0;
 evenDivisors = [];
 let sumEvenDivisors = 0;
-for (let x = 0; x <= N; x++) {
-    if ((N % x !== 0) || (x % 2 !== 0)) {
+let l = 0;
+for (let i = 0; i <= N; i++) {
+    if ((N % i !== 0) || (i % 2 !== 0)) {
         continue;
     }
-    evenDivisors.push(x);
-    for (; n < evenDivisors.length; n++) {
-        sumEvenDivisors += evenDivisors[n];
+    evenDivisors.push(i);
+    while (l < evenDivisors.length) {
+        sumEvenDivisors += evenDivisors[l];
+        l++;
+    }
 }
-}
-alert("Сума парних дільників натурального числа «" + N + "»:\n" +sumEvenDivisors);
+alert("Сума парних дільників натурального числа «" + N + "»:\n" + sumEvenDivisors);
 
-
-// Завдання 12
-// Надрукувати повну таблицю множення від 1 до 10.
-i = 1;
-n = 1;
-let multTable = [];
-for(; i <= 10; i++) {
-for (n = 1; n <= 10; n++) {
-    multTable.push(i + "*" + n +"=" + (i*n));  
-}};
-alert("Повна таблиця множення:\n" +multTable.join("; "));
 
 // Завдання 9, 10, 11. 
 // Варіант 2: коли число задається на початку і подальші цикли прораховують значення враховуючі, результати попередніх циклів.
 // Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-i = 0;
-N = 24;
 divisors = [];
-for (; i <= N; i++) {
+for (let i = 0; i <= N; i++) {
     if ((N % i) !== 0) {
         continue;
     }
@@ -158,16 +140,27 @@ for (; i <= N; i++) {
 alert("Дільники натурального числа «" + N + "»:\n" + divisors);
 // Визначити кількість його парних дільників.
 evenDivisors = [];
-for (let x = 0; x <= divisors.length; x++) {
-    if (divisors[x] % 2 !== 0) {
+for (let i = 0; i <= divisors.length; i++) {
+    if (divisors[i] % 2 !== 0) {
         continue;
     }
-    evenDivisors.push(divisors[x]);
+    evenDivisors.push(divisors[i]);
 }
 alert("Кількість парних дільників числа «" + N + "»:\n" + evenDivisors.length);
 // Знайти суму його парних дільників.
 sumEvenDivisors = 0;
-for(let y = 0; y < evenDivisors.length; y++) {
-    sumEvenDivisors += evenDivisors[y];
+for (let i = 0; i < evenDivisors.length; i++) {
+    sumEvenDivisors += evenDivisors[i];
 }
-alert("Сума парних дільників натурального числа «" + N + "»:\n" +sumEvenDivisors);
+alert("Сума парних дільників натурального числа «" + N + "»:\n" + sumEvenDivisors);
+
+
+// Завдання 12
+// Надрукувати повну таблицю множення від 1 до 10.
+let multTable = [];
+for (let i = 1; i <= 10; i++) {
+    for (let n = 1; n <= 10; n++) {
+        multTable.push(i + "*" + n + "=" + (i * n));
+    }
+};
+alert("Повна таблиця множення:\n" + multTable.join("; "));
