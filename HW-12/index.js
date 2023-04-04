cdc5028c02aed799e3be5346c8a73834dbb6f85c
AdminK <volvenkova.k@gmail.com>
2023-04-04 23:13:57 +0300

    correct hw-12,14

--- HW-12/index.js
+++ HW-12/index.js
@@ -1,20 +1,21 @@
-// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
-// const array = [1, 2, 3, 4, 5, 6, 7];
-// removeElement(array, 5 );
-// console.log(array);
-// Результат: [1, 2, 3, 4, 6, 7]
+/*Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5 );
+console.log(array);
+Результат: [1, 2, 3, 4, 6, 7]*/
 
 const array = [1, 2, 3, 4, 5, 6, 7];
 let removeElement = function (array, item) {
     let n = array.length;
     for (let i = 0; i < n; i++) {
-        if (array[0] === item) { 
+        if (array[0] === item) {
             array.shift();
             continue;
         }
         array.push(array[0]);
-        array.shift(); 
+        array.shift();
     }
-    console.log(array)
+    return array;
 }
-removeElement(array, 5)
+removeElement(array, 5);
+console.log(array);
