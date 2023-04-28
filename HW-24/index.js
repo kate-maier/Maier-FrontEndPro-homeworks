5bfbdca529e728d53e4ea55fec1be44cad6de083
AdminK <volvenkova.k@gmail.com>
2023-04-28 14:35:01 +0300

    correct hw-24

--- HW-24/index.js
+++ HW-24/index.js
@@ -119,7 +119,7 @@ class Hamburger {
 }
 
 // маленький гамбургер з начинкою з сиру
-var hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
 
 // добавка з майонезу
 hamburger.addTopping(Hamburger.TOPPING_MAYO);
